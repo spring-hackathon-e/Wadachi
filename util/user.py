20f619de903f37df587fbe940c6b6eabed959042
やぶ <100475245+kaimugura@users.noreply.github.com>
2023-04-22 12:04:18 +0900

    Update user.py

--- util/user.py
+++ util/user.py
@@ -1,12 +1,12 @@
 class User:
-    def __init__(self, uid, name, email, password):
-        self.uid = uid
-        self.name = name
+    def __init__(self, user_id, user_name, email, password):
+        self.user_id = user_id
+        self.user_name = user_name
         self.email = email
         self.password = password
     
     def getUserName(self):
-        return self.name
+        return self.user_name
 
     def getUserEmail(self):
         return self.email
