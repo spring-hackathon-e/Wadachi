056d9b185f0343794b2a7114f5f05de35c2ff3e9
やぶ <100475245+kaimugura@users.noreply.github.com>
2023-04-23 18:11:36 +0900

    サインイン機能に関する追記になります。
    進捗として追記します。

---
+++ models.py
@@ -0,0 +1,44 @@
+import pymysql
+from util.DB import DB
+
+class dbConnect:
+    def createUser(user): ##ユーザ情報追加
+        try:
+            connect = DB.getConnection()
+            cursor = connect.cursor()
+            sql = "INSERT INTO users (user_id, user_name, email, user.password) VALUES (%s,%s,%s,%s);"
+            cursor.execute(sql, (user.user_id, user.user_name, user.email, user.password))
+            connect.commit()
+        except Exception as e:
+            print(e + 'が発生しています。')
+            return None
+        finally:
+            cursor.close()
+
+    def getUser(email): #emailが登録済みか判別
+        try: 
+            connect = DB.getConnection()
+            cursor = connect.cursor()
+            sql = "SELECT email FROM users WHERE email=%s" ##app.pyでgetしたemail
+            cursor.execute(sql, (email,))
+            reg_email = cursor.fetchone()
+            return reg_email ##登録済みのemailを返す
+        except Exception as e: ##エラーの見える化
+            print(e + 'が発生しています。')
+            return None ##未登録であることを返す。
+        finally:
+            cursor.close()
+
+    def getUserId(user_id):
+        try:
+            connect = DB.getConnection()
+            cursor = connect.cursor()
+            sql = "SELECT user_id FROM users WHERE user_id=%s"
+            cursor.execute(sql, (user_id,))
+            reg_user_id = cursor.fetchone()
+            return reg_user_id
+        except Exception as e: ##エラーの見える化
+            print(e + 'が発生しています。')
+            return None ##未登録であることを返す。
+        finally:
+            cursor.close()
