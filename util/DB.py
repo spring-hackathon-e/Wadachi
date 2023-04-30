939dd2c66e672f813c38bd5d5b87c878bc6fe552
kit06 <hts6573@gmail.com>
2023-04-30 12:14:40 +0900

    util/DB.py

---
+++ util/DB.py
@@ -0,0 +1,19 @@
+import pymysql
+
+class DB:
+    def getConnection():
+        #例外処理
+        try:
+            conn = pymysql.connect(
+                host="localhost",
+                db="Wadachi",
+                user="gruper",
+                password="spring_e",
+                charset="utf8",
+                cursorclass=pymysql.cursors.DictCursor
+            )
+            return conn
+        #エラー処理
+        except(ConnectionError):
+            print("コネクションエラーです")
+            conn.close() #DB接続を閉じる
