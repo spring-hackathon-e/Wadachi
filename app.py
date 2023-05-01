dbe482a3ce45b9d33f187fb612108db9b642a303
やぶ <100475245+kaimugura@users.noreply.github.com>
2023-05-01 23:46:34 +0900

    認証機能の追加です。

--- app.py
+++ app.py
@@ -11,12 +11,12 @@ app.secret_key = uuid.uuid4().hex
 app.permanent_session_lifetime = timedelta(days=30)
 
 #サインイン
-@app.route('/signin')
+@app.route('/signup')
 def signup():
-    return render_template('registration/signin.html')
+    return render_template('registration/signup.html')
 
-@app.route('/signin', methods=['POST'])
-def signup(): #登録情報の取得
+@app.route('/signup', methods=['POST'])
+def usersignup():   # 登録情報の取得
     user_name = request.form.get('user_name')
     email = request.form.get('email')
     user_id = request.form.get('name')
@@ -26,7 +26,6 @@ def signup(): #登録情報の取得
     def isMail(email):
         pattern = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
         return re.match(pattern, email)
-
 
     if user_id == '' or email =='' or password == '' or password_chk == '':
         flash('空のフォームがあります')
@@ -38,13 +37,45 @@ def signup(): #登録情報の取得
         user_id = uuid.uuid4
         password = hashlib.sha256(password.encode('utf-8')).hexdigest()
         user = User(user_id, user_name, email, password)
-        DBuser = dbConnect.getUser(email)
+        DbUserMail = dbConnect.getUser(email)
+        DbUserId = dbConnect.getUser(user_id)
 
-    if DBuser != None: #emailが登録済み
+    if DbUserMail != None:   # emailが登録済み
+        flash('すでに登録済みのユーザーです。')
+    elif DbUserId != None:
         flash('すでに登録済みのユーザーです。')
     else:
         dbConnect.createUser(user)
         UserId = str(user_id)
         session['user_id'] = UserId
         return redirect('/')
-    return redirect('/signup')  #間違ってる所をクリア、色塗りの方がいいんじゃ
+    return redirect('/signup')   # 間違ってる所をクリア、色塗りの方がいいんじゃ
+
+#ログイン
+@app.route('/login')
+def login():
+    return render_template('registration/login.html')
+
+@app.route('/login', methods=['POST'])
+def userlogin():   # user_idとemailを格納先と照合
+    user_id = request.form.get('user_id')
+    password = request.form.get('password')
+    
+    user = dbConnect.getUserId(user_id)
+
+    if user == None:
+        flash('ユーザーIDが間違っています。')
+    else:
+        hashpassword = hashlib.sha256(password.encode('utf-8')).hexdigest()
+        if hashpassword != user[password] :
+            flash('パスワードが間違っています。')
+        else:
+            session['user_id'] = user["user_id"]
+            return redirect('/')
+    return redirect('/login')
+
+#ログアウト
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/login')
