98310ab9c8b366dbd7c4a530dd65e8ca42fda6cc
やぶ <100475245+kaimugura@users.noreply.github.com>
2023-05-01 23:48:33 +0900

    認証機能の追加

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ import pymysql
 from util.DB import DB
 
 class dbConnect:
-    def createUser(user): ##ユーザ情報追加
+    def createUser(user):   # ユーザ情報追加
         try:
             connect = DB.getConnection()
             cursor = connect.cursor()
@@ -10,35 +10,37 @@ class dbConnect:
             cursor.execute(sql, (user.user_id, user.user_name, user.email, user.password))
             connect.commit()
         except Exception as e:
-            print(e + 'が発生しています。')
+            print(str(e) + 'が発生しています。')
             return None
         finally:
             cursor.close()
 
-    def getUser(email): #emailが登録済みか判別
+    def getUser(email):   # emailが登録済みか判別
         try: 
             connect = DB.getConnection()
             cursor = connect.cursor()
-            sql = "SELECT email FROM users WHERE email=%s" ##app.pyでgetしたemail
+            sql = "SELECT email FROM users WHERE email=%s"   #app.pyでgetしたemail
             cursor.execute(sql, (email,))
-            reg_email = cursor.fetchone()
-            return reg_email ##登録済みのemailを返す。なければNoneを返す。
-        except Exception as e: ##エラーの見える化
-            print(e + 'が発生しています。')
-            return None
+            email = cursor.fetchone()
+            return email   # 登録済みのemailを返す。無ければNone
+        except Exception as e:   #エラーの見える化
+            print(str(e) + 'が発生しています。')
+            return None 
         finally:
             cursor.close()
 
-    def getUserId(user_id):　#user_idが登録済か判別
+    def getUserId(user_id):
         try:
             connect = DB.getConnection()
             cursor = connect.cursor()
-            sql = "SELECT user_id FROM users WHERE user_id=%s"
+            sql = "SELECT * FROM users WHERE user_id=%s"
             cursor.execute(sql, (user_id,))
-            reg_user_id = cursor.fetchone()
-            return reg_user_id
-        except Exception as e: ##エラーの見える化
+            user_id = cursor.fetchone()
+            return user_id
+        except Exception as e:   # エラーの見える化
             print(e + 'が発生しています。')
             return None
         finally:
             cursor.close()
+        
+    
