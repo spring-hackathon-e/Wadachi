b7c7819427eb2274405de2697ed42af3d781a21e
やぶ <100475245+kaimugura@users.noreply.github.com>
2023-05-02 13:02:00 +0900

    軽微な修正を行いました。
    下記エラーが発生しており、サインインできない状態です。
    UnboundLocalError: cannot access local variable 'cur' where it is not associated with a value

--- models.py
+++ models.py
@@ -1,13 +1,15 @@
 import pymysql
 from util.DB import DB
 
+
 class dbConnect:
     def createUser(user):   # ユーザ情報追加
         try:
             connect = DB.getConnection()
             cursor = connect.cursor()
             sql = "INSERT INTO users (user_id, user_name, email, user.password) VALUES (%s,%s,%s,%s);"
-            cursor.execute(sql, (user.user_id, user.user_name, user.email, user.password))
+            cursor.execute(sql, (user.user_id, user.user_name,
+                           user.email, user.password))
             connect.commit()
         except Exception as e:
             print(str(e) + 'が発生しています。')
@@ -15,32 +17,30 @@ class dbConnect:
         finally:
             cursor.close()
 
-    def getUser(email):   # emailが登録済みか判別
-        try: 
-            connect = DB.getConnection()
-            cursor = connect.cursor()
-            sql = "SELECT email FROM users WHERE email=%s"   #app.pyでgetしたemail
-            cursor.execute(sql, (email,))
-            email = cursor.fetchone()
-            return email   # 登録済みのemailを返す。無ければNone
-        except Exception as e:   #エラーの見える化
-            print(str(e) + 'が発生しています。')
-            return None 
-        finally:
-            cursor.close()
-
-    def getUserId(user_id):
+    def getUserId(email):   # emailが登録済みか判別
         try:
             connect = DB.getConnection()
             cursor = connect.cursor()
-            sql = "SELECT * FROM users WHERE user_id=%s"
-            cursor.execute(sql, (user_id,))
-            user_id = cursor.fetchone()
-            return user_id
+            sql = "SELECT user_id FROM users WHERE email=%s"   # app.pyでgetしたemail
+            cursor.execute(sql, (email,))
+            id = cursor.fetchone()
+            return id   # 登録済みのemailを返す。無ければNone
         except Exception as e:   # エラーの見える化
-            print(e + 'が発生しています。')
+            print(str(e) + 'が発生しています。')
             return None
         finally:
-            cursor.close()
-        
-    
+            cursor.close
+
+    def getUser(email):
+        try:
+            connect = DB.getConnection()
+            cursor = connect.cursor()
+            sql = "SELECT * FROM users WHERE email=%s"
+            cursor.execute(sql, (email,))
+            user = cursor.fetchone()
+            return user
+        except Exception as e:   # エラーの見える化
+            print(str(e) + 'が発生しています。')
+            return None
+        finally:
+            cursor.close
