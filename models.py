f25fa6546551f713a3512f01f95597659cb6a8a2
Duvke <l.heize.369@gmail.com>
2023-05-04 20:35:32 +0900

    models.py修正

--- models.py
+++ models.py
@@ -45,96 +45,96 @@ class dbConnect:
 
     def getChannelAll():# チャンネル一覧機能
         try:
-            conn = pymysql.getConnection()
-            cur = conn.cursor()
+            connect = DB.getConnection()
+            cursor = connect.cursor()
             sql = "SELECT * FROM channels;"
-            cur.execute(sql)
-            channels = cur.fetchall()
+            cursor.execute(sql)
+            channels = cursor.fetchall()
             return channels
         except Exception as e:
             print(e + 'が発生しています')
             return None
         finally:
-            cur.close()
+            cursor.close()
 
 
     def getChannelById(ch_id):# チャンネル作成機能
         try:
-            conn = pymysql.getConnection()
-            cur = conn.cursor()
+            connect = DB.getConnection()
+            cursor = connect.cursor()
             sql = "SELECT * FROM channels WHERE id=%s;"
-            cur.execute(sql, (ch_id))
-            channel = cur.fetchone()
+            cursor.execute(sql, (ch_id))
+            channel = cursor.fetchone()
             return channel
         except Exception as e:
             print(e + 'が発生しています')
             return None
         finally:
-            cur.close()
+            cursor.close()
 
 
     def getChannelByName(ch_name):
         try:
-            conn = pymysql.getConnection()
-            cur = conn.cursor()
+            connect = DB.getConnection()
+            cursor = connect.cursor()
             sql = "SELECT * FROM channels WHERE name=%s;"
-            cur.execute(sql, (ch_name))
-            channel = cur.fetchone()
+            cursor.execute(sql, (ch_name))
+            channel = cursor.fetchone()
             return channel
         except Exception as e:
             print(e + 'が発生しています')
             return None
         finally:
-            cur.close()
+            cursor.close()
 
 
     def addChannel(user_id, newCh_Name, newChannel_summary):
         try:
-            conn = pymysql.getConnection()
-            cur = conn.cursor()
+            connect = DB.getConnection()
+            cursor = connect.cursor()
             sql = "INSERT INTO channels (user_id, name, abstract) VALUES (%s, %s, %s);"
-            cur.execute(sql, (user_id, newCh_Name, newChannel_summary))
-            conn.commit()
+            cursor.execute(sql, (user_id, newCh_Name, newChannel_summary))
+            connect.commit()
         except Exception as e:
             print(e + 'が発生しています')
             return None
         finally:
-            cur.close()
+            cursor.close()
 
 
     def getChannelByName(ch_name):
         try:
-            conn = pymysql.getConnection()
-            cur = conn.cursor()
+            connect = DB.getConnection()
+            cursor = connect.cursor()
             sql = "SELECT * FROM channels WHERE name=%s;"
-            cur.execute(sql, (ch_name))
-            channel = cur.fetchone()
+            cursor.execute(sql, (ch_name))
+            channel = cursor.fetchone()
         except Exception as e:
             print(e + 'が発生しました')
             return None
         finally:
-            cur.close()
+            cursor.close()
             return channel
 
 
     def updateChannel(user_id, newCh_Name, newChannel_summary, ch_id): # チャンネル編集機能
-        conn = pymysql.getConnection()
-        cur = conn.cursor()
+        connect = DB.getConnection()
+        cursor = connect.cursor()
         sql = "UPDATE channels SET user_id=%s, name=%s, abstract=%s WHERE id=%s;"
-        cur.execute(sql, (user_id, newCh_Name, newChannel_summary, ch_id))
-        conn.commit()
-        cur.close()
+        cursor.execute(sql, (user_id, newCh_Name, newChannel_summary, ch_id))
+        connect.commit()
+        cursor.close()
 
-    def deleteChannel(cid): # チャンネル削除機能
-        try: 
-            conn = pymysql.getConnection()
-            cur = conn.cursor()
+    def deleteChannel(ch_id): # チャンネル削除機能
+        try:
+            connect = DB.getConnection()
+            cursor = connect.cursor()
             sql = "DELETE FROM channels WHERE id=%s;"
-            cur.execute(sql, (ch_id))
-            conn.commit()
+            cursor.execute(sql, (ch_id))
+            connect.commit()
         except Exception as e:
             print(e + 'が発生しています')
             return None
         finally:
-            cur.close()
+            cursor.close()
 
