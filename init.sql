c5576f5b293884dd4931e58500885210010ad6dd
やぶ <100475245+kaimugura@users.noreply.github.com>
2023-05-05 18:15:42 +0900

    5/5 18時時点の情報を反映

--- init.sql
+++ init.sql
@@ -3,27 +3,46 @@ DROP USER 'gruper'@'localhost';
 
 CREATE USER 'gruper'@'localhost' IDENTIFIED BY 'Spring_e6';
 CREATE DATABASE Wadachi;
-USE Wadachi
+USE Wadachi;
 GRANT ALL PRIVILEGES ON Wadachi.* TO 'gruper'@'localhost';
 
 CREATE TABLE users (
-    uid varchar(255) PRIMARY KEY,
-    name varchar(255) UNIQUE NOT NULL,
-    email varchar(255) UNIQUE NOT NULL,
-    password varchar(255) NOT NULL
+    user_id VARCHAR(255) PRIMARY KEY NOT NULL,
+    user_name VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(250) NOT NULL
+
 );
 
 CREATE TABLE channels (
-    id serial PRIMARY KEY,
-    uid varchar(255) REFERENCES users(uid),
-    name varchar(255) UNIQUE NOT NULL,
-    abstract varchar(255)
+    ch_id SERIAL PRIMARY KEY,
+    user_id VARCHAR(255) REFERENCES users(user_id),
+    ch_name VARCHAR(255) UNIQUE NOT NULL,
+    summary VARCHAR(255),
+    main_category VARCHAR(255),
+    sub_category VARCHAR(255),
+    created_date TIMESTAMP NOT NULL default current_timestamp
 );
 
 CREATE TABLE messages (
-    id serial PRIMARY KEY,
-    uid varchar(255) REFERENCES users(uid),
-    cid integer REFERENCES channels(id) ON DELETE CASCADE,
-    message text,
-    created_at timestamp not null default current_timestamp
+    message_id SERIAL PRIMARY KEY,
+    user_id VARCHAR(255) REFERENCES users(user_id),
+    ch_id INT REFERENCES channel(ch_id) ON DELETE CASCADE,
+    message TEXT,
+    created_date TIMESTAMP NOT NULL default current_timestamp,
+    reaction INT
 );
+
+CREATE TABLE posts (
+    post_id SERIAL PRIMARY KEY,
+    user_id VARCHAR(255) REFERENCES users(user_id),
+    title VARCHAR(255) NOT NULL,
+    post TEXT,
+    created_date TIMESTAMP NOT NULL default current_timestamp,
+    study_time INT
+);
+
+
+INSERT INTO users(user_id, user_name, email, password)VALUES('970af84c-dd40-47ff-af23-282b72b7cca8','test','test@gmail.com','37268335dd6931045bdcdf92623ff819a64244b53d0e746d438797349d4da578');
+# INSERT INTO channels(id, uid, name, abstract)VALUES(1, '970af84c-dd40-47ff-af23-282b72b7cca8','botti','テストさんの孤独な部屋です');
+# INSERT INTO messages(id, uid, cid, message)VALUES(1, '970af84c-dd40-47ff-af23-282b72b7cca8', '1', '誰かかまってください、、');
