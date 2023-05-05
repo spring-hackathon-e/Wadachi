10015645ae74771da7b3117cf1d815423805ddf5
やぶ <100475245+kaimugura@users.noreply.github.com>
2023-05-05 18:18:27 +0900

    5/5 18時時点の情報を反映

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, redirect, render_template, session, flash
 from models import dbConnect
 from util.user import User
 from datetime import timedelta
+from itsdangerous.url_safe import URLSafeTimedSerializer  # リマインド機能
 import hashlib
 import uuid
 import re
@@ -10,10 +11,12 @@ app = Flask(__name__)
 app.secret_key = uuid.uuid4().hex
 app.permanent_session_lifetime = timedelta(days=30)
 
-#サインアップ
+
+# サインアップ
 @app.route('/signup')
 def signup():
     return render_template('registration/signup.html')
+
 
 @app.route('/signup', methods=['POST'])
 def usersignup():   # 登録情報の取得
@@ -22,22 +25,20 @@ def usersignup():   # 登録情報の取得
     password = request.form.get('password1')
     password_chk = request.form.get('password2')
 
-    def isMail(email):
-        pattern = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
-        return re.match(pattern, email)
+    pattern = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
 
-    if user_name == '' or email =='' or password == '' or password_chk == '':
+    if user_name == '' or email == '' or password == '' or password_chk == '':
         flash('空のフォームがあります')
-    elif password != password_chk :
+    elif password != password_chk:
         flash('パスワードが一致していません。')
-    elif isMail(email) is None:
+    elif re.match(pattern, email) is None:
         flash('正しいメールアドレスを記入してください。')
     else:
         user_id = uuid.uuid4
         password = hashlib.sha256(password.encode('utf-8')).hexdigest()
         user = User(user_id, user_name, email, password)
         DbUser = dbConnect.getUser(email)
- 
+
         if DbUser != None:   # emailが登録済み
             flash('すでに登録済みのユーザーです。')
         else:
@@ -47,39 +48,66 @@ def usersignup():   # 登録情報の取得
             return redirect('/')
     return redirect('/signup')   # 入力情報のクリア
 
-#ログイン
+
+# ログイン
 @app.route('/login')
 def login():
     return render_template('registration/login.html')
 
+
 @app.route('/login', methods=['POST'])
-def userlogin():   # user_idとemailを格納先と照合
+def userlogin():  # user_idとemailを格納先と照合
     email = request.form.get('email')
     password = request.form.get('password')
-    
+
     user = dbConnect.getUser(email)
 
     if user == None:
         flash('ユーザーIDが間違っています。')
     else:
         hashpassword = hashlib.sha256(password.encode('utf-8')).hexdigest()
-        if hashpassword != user[password] :
+        if hashpassword != user["password"]:
             flash('パスワードが間違っています。')
         else:
             session['user_id'] = user["user_id"]
             return redirect('/')
     return redirect('/login')
 
-#ログアウト
+
+# ログアウト
 @app.route('/logout')
 def logout():
     session.clear()
     return redirect('/login')
 
-# パスワードリマインド機能
+
+# パスワードリマインド機能　作成中です。
 @app.route('/remind')
+# def create_token(user_id, secret_key, salt):
+#    serializer = URLSafeTimedSerializer(secret_key)
+#    return serializer.dumps(user_id, salt=salt)
+# remind.htmlでの動作
 def remind():
     return render_template('registration/remind.html')
+
+
+@app.route('/remind', methods=['POST'])
+def userremind():
+    email = request.form.get('reg_email')
+    DbUser = dbConnect.getUser(email)
+    if email == '':
+        flash('メールアドレスを入力してください。')
+    elif DbUser == None:
+        flash('メールアドレスは登録されていません。')
+
+    else:  # リマインドメール送付
+        print('送付完了')  # 仮でおいてます。
+
+        return redirect('/login')
+    return redirect('/remind')
+
+
+# ユーザー削除
 
 #app.run
 if __name__ == '__main__':
