eba749f5b5638510600b3e305d720ebe451c6bb4
やぶ <100475245+kaimugura@users.noreply.github.com>
2023-05-05 18:12:26 +0900

    最新の状態を反映

---
+++ util/DB.py
@@ -0,0 +1,18 @@
+import pymysql
+
+
+class DB:
+    def getConnection():
+        try:
+            conn = pymysql.connect(
+                host="localhost",
+                db="Wadachi",
+                user="gruper",
+                password="Spring_e6",
+                charset="utf8",
+                cursorclass=pymysql.cursors.DictCursor
+            )
+            return conn
+        except (ConnectionError):
+            print("コネクションエラーです")
+            conn.close()
