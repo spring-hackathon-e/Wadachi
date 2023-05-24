78a91cebd751efdc2cfecc9846444fb6d8e3a327
やぶ <100475245+kaimugura@users.noreply.github.com>
2023-05-24 22:42:15 +0900

    Update init.sql

--- init.sql
+++ init.sql
@@ -6,17 +6,15 @@ CREATE DATABASE Wadachi;
 USE Wadachi
 GRANT ALL PRIVILEGES ON Wadachi.* TO 'gruper'@'localhost';
 
---　usersテーブルの作成
 CREATE TABLE users (
-    user_id VARCHAR(255) PRIMARY KEY, NOT NULL,
-    user_name VARCHAR(255) UNIQUE, NOT NULL,
-    email VARCHAR(255) UNIQUE, NOT NULL,
+    user_id VARCHAR(255) PRIMARY KEY NOT NULL,
+    user_name VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     goal TEXT,
-    start_date DATE, NOT NULL
+    start_date DATE NOT NULL
 );
 
---　channelsテーブルの作成
 CREATE TABLE channels (
     ch_id SERIAL PRIMARY KEY,
     user_id VARCHAR(255) REFERENCES users(user_id),
@@ -24,25 +22,24 @@ CREATE TABLE channels (
     summary VARCHAR(255),
     main_category VARCHAR(255),
     sub_category VARCHAR(255),
-    created_date TIMESTAMP not null default current_timestamp
+    created_date TIMESTAMP NOT NULL default current_timestamp
 );
 
---　messagesテーブルの作成
 CREATE TABLE messages (
     message_id SERIAL PRIMARY KEY,
     user_id VARCHAR(255) REFERENCES users(user_id),
     ch_id INT REFERENCES channel(ch_id) ON DELETE CASCADE,
     message TEXT,
-    created_date TIMESTAMP not null default current_timestamp,
+    created_date TIMESTAMP NOT NULL default current_timestamp,
     reaction INT
 );
 
---　postsテーブルの作成
 CREATE TABLE posts (
     post_id SERIAL PRIMARY KEY,
     user_id VARCHAR(255) REFERENCES users(user_id),
     title VARCHAR(255) NOT NULL,
     post TEXT,
-    created_date TIMESTAMP not null default current_timestamp,
+    created_date TIMESTAMP NOT NULL default current_timestamp,
     study_time INT
+    reaction INT
 );
